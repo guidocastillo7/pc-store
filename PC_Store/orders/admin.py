5355d5a0557294eb7f38e3798ee31fd69b4c69ee
Guido Castillo <guido.castillo.002@gmail.com>
2024-12-20 13:56:57 +0100

    Agregando modelos al panel admin

--- PC_Store/orders/admin.py
+++ PC_Store/orders/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Order, OrderDetail
 
-# Register your models here.
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = (
+        "customer",
+        "order_number",
+        "status",
+        "created_at"
+    )
+
+
+@admin.register(OrderDetail)
+class OrderDetailAdmin(admin.ModelAdmin):
+    list_display = (
+        "order",
+        "product",
+        "quantity",
+        "amount"
+    )
