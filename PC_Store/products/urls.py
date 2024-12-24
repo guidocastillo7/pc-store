15ea7f20f3fe8b2e59cc71fe23dbd1ca977d8aea
Guido Castillo <guido.castillo.002@gmail.com>
2024-12-24 18:54:20 +0100

    Vista del producto individual

--- PC_Store/products/urls.py
+++ PC_Store/products/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.products, name="products"),
+    path("<int:product_id>", views.products, name="products"),
 ]
--- PC_Store/products/views.py
+++ PC_Store/products/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from .models import Product
 
 
-def products(request):
-    return render(request, "products.html")
+def products(request, product_id):
+    product = Product.objects.get(pk=product_id)
+
+    context = {
+        "product": product
+    }
+
+    return render(request, "products.html", context)
