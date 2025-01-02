8de8c2a46811d20dd0eef84408f8eeddbc5009fb
Guido Castillo <guido.castillo.002@gmail.com>
2025-01-02 12:04:58 +0100

    Arreglo de error del form de registro con relacion al modelo User

--- PC_Store/users/forms.py
+++ PC_Store/users/forms.py
@@ -24,3 +24,8 @@ class UserRegisterForm(UserCreationForm):
         self.fields["username"].widget.attrs.update({"class":"form-control"})
         self.fields["password1"].widget.attrs.update({"class":"form-control"})
         self.fields["password2"].widget.attrs.update({"class":"form-control"})
+
+    class Meta:
+        model = User
+        fields = ["username", "email", "first_name",
+                  "last_name", "password1", "password2"]
