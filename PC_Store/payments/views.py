aa5a77ef58b6238ceff79b5ab91512867fafdeb0
Guido Castillo <guido.castillo.002@gmail.com>
2025-01-08 11:58:31 +0100

    Configurando pagina de checkout

--- PC_Store/payments/views.py
+++ PC_Store/payments/views.py
@@ -1,5 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import (render, redirect)
+from django.contrib.auth.decorators import login_required
+from cart.cart import Cart
 
 
+@login_required(login_url="/users/login")
 def checkout(request):
-    return render(request, "checkout.html")
+    if not request.session.get("cart"):
+        return redirect("/cart")
+
+    address = request.user.address
+    cart = Cart(request)
+
+    context = {
+        "address": address,
+        "total_amount": cart.get_total_amount()
+    }
+
+    return render(request, "checkout.html", context)
